ef75dd525238a8b39ceac254ad22e5b8f60ded9c
fefe <felix.parendeau@gmail.com>
2026-03-23 16:03:09 +0100

    add folder and file pageCreateRenseignementh.html

---
+++ assets/script/creationQuestionnaire.js
@@ -0,0 +1,7 @@
+const containerQuestionVideo = document.querySelector(
+  "#container-questions-video",
+);
+
+for (let i = 1; i < 15; i++) {
+  const divQuestionVideo = document.createElement("div");
+}
--- creationQuestionnaire.js
+++
@@ -1,7 +0,0 @@
-const containerQuestionVideo = document.querySelector(
-  "#container-questions-video",
-);
-
-for (let i = 1; i < 15; i++) {
-  const divQuestionVideo = document.createElement("div");
-}
