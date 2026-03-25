dc84fe3bbe82c48ed1b7a190f897da5d40a5c969
fefe <felix.parendeau@gmail.com>
2026-03-25 14:37:19 +0100

    fix bug

--- assets/script/pageCreateRenseignements.js
+++ assets/script/pageCreateRenseignements.js
@@ -1,11 +1,37 @@
 const btnSubmit = document.getElementById("btnSubmit");
-btnSubmit.addEventListener("click", (event) => {
-  event.preventDefault();
-  window.location.href = "allQuestionnaire.html";
-});
 
 const btnBack = document.getElementById("btnBack");
 btnBack.addEventListener("click", (event) => {
   event.preventDefault();
   window.location.href = "../index.html";
 });
+
+function saveJsonChildren() {
+  btnSubmit.addEventListener("click", (event) => {
+    const numberPassation = document
+      .querySelector("#number-passation")
+      .value.trim();
+    const age = document.querySelector("#age").value.trim();
+    const sexe = document.querySelector("#sexe-select").value.trim();
+    event.preventDefault();
+    const childrenDatas = {
+      numberPassation: numberPassation,
+      age: age,
+      sexe: sexe,
+    };
+    downloadJson(childrenDatas);
+    window.location.href = "allQuestionnaire.html";
+  });
+}
+
+function downloadJson(childrenDatas) {
+  window.electronAPI.sendData2(childrenDatas);
+  alert("Le questionnaire a été enregistré dans tes Documents !");
+}
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "allQuestionnaire.html";
+});
+
+saveJsonChildren();
