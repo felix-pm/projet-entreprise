2a53ca04aa77890325b2e5a807dc6a36051dd52c
fefe <felix.parendeau@gmail.com>
2026-03-26 14:39:06 +0100

    tiens eliot

--- assets/script/preload.js
+++ assets/script/preload.js
@@ -1,12 +1,9 @@
-// preload.js
-import { contextBridge, ipcRenderer } from "electron";
+const { contextBridge, ipcRenderer } = require("electron");
 
-// On crée notre fameux canal sécurisé "electronAPI"
 contextBridge.exposeInMainWorld("electronAPI", {
-  // On crée une fonction qui envoie les données au cerveau (Node.js)
-  // On étiquette ce message "nouveau-formulaire"
   sendData: (data) => ipcRenderer.send("form-test", data),
   sendData2: (childrenDatas) =>
     ipcRenderer.send("form-renseignements", childrenDatas),
+
   getQuestionnaires: () => ipcRenderer.invoke("get-questionnaires"),
 });
