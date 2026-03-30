1ce549d868933d50c83333872cc30abb7d150e06
fefe <felix.parendeau@gmail.com>
2026-03-30 11:53:10 +0200

    add btnBack on scoreExterieurs

--- assets/script/FrontEnd/pushYield2.js
+++ assets/script/FrontEnd/pushYield2.js
@@ -1,3 +1,9 @@
+const btnBack = document.getElementById("btnBack");
+
+btnBack.addEventListener("click", (event) => {
+  window.location.href = "questionnaire.html";
+});
+
 const yield2 = {};
 
 const buttonRecordScore = document.getElementById("recordScore");
