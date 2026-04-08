c7755d6a74e4dba1630648bda42999c4b972c53b
PierreSOREAU <soreaupierre@gmail.com>
2026-04-08 09:47:45 +0200

    feat-popup excel

--- assets/script/FrontEnd/jsonToExcel.js
+++ assets/script/FrontEnd/jsonToExcel.js
@@ -5,6 +5,17 @@ function jsonToExcel() {
   buttonToExcel.textContent = "Enregistrer en excel 📊";
   buttonToExcel.addEventListener("click", () => {
     window.electronAPI.generateExcel(titleJSON);
+    const newdiv = document.createElement("div");
+    const newp = document.createElement("p");
+    const body = document.querySelector("body");
+    body.append(newdiv);
+    newdiv.append(newp);
+    newp.textContent = "Document excel enregistré dans Téléchargements";
+    newdiv.classList.add("download");
+
+    setTimeout(() => {
+      newdiv.style.display = "none";
+    }, 3000);
   });
 }
 
